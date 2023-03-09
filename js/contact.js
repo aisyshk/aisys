2082200a236cb078de5138cb3028be073a9baf9f
無冬 <123821243+aisyshk@users.noreply.github.com>
2023-03-09 01:53:37 -0700

    redesigned some pages

--- js/contact.js
+++ js/contact.js
@@ -4,6 +4,7 @@ let name = document.getElementById('name');
 let mail = document.getElementById('mail');
 let contactReason = document.getElementById('contactReason');
 let subject = document.getElementById('subject');
+let date = Date.now().toString();
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -14,23 +15,9 @@ form.addEventListener('submit', (e) => {
         contactReason: contactReason.value,
         subject: subject.value
     }
-
     console.log(formData);
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/');
-    xhr.setRequestHeader('content', 'application/json');
-    xhr.onload = function () {
-        console.log(xhr.responseText);
-
-        if (xhr.responseText == 'success') {
-            alert('Email sent');
-            name.value = '';
-            mail.value = '';
-            contactReason.value = '';
-            subject.value = '';
-        }
-    }
-
-    xhr.send(JSON.stringify(formData));
+    var fileToSave = new Blob([JSON.stringify(formData)], {
+        type: 'application/json'
+    });
 })
--- js/script.js
+++ js/script.js
@@ -2,16 +2,70 @@ function showDropdown() {
     document.getElementById("servicesDropdown").classList.toggle("show");
 }
 
-window.onclick = function (event) {
-    if (!event.target.matches('.nav-dropdown-button')) {
-        var dropdowns = document.getElementsByClassName('dropdown-content');
+function displayWorkPanel() {
+    if (document.getElementById("work-panel").style.display == "block") {
+        document.getElementById("work-panel").style.display = "none";
+    }
+    else {
+        document.getElementById("work-panel").style.display = "block";
+    }
+}
 
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i]
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+function displaySection(sectionId) {
+    var secs = document.getElementsByClassName("info-section");
+    var otherSecs = [];
+
+    for (var i = 0; i < secs.length; i++) {
+
+        if (secs[i].id == sectionId) {
+            if (secs[i].style.display == "block") {
+                //secs[i].style.display = "none";
             }
+            else {
+                secs[i].style.display = "block";
+            }
+        }
+        else {
+            otherSecs.push(secs[i]);
+            for (var j = 0; j < otherSecs.length; j++) {
+                otherSecs[j].style.display = "none";
+            }
+        }
+    }
+}
+
+function displayPanel(panelId) {
+    var panels = document.getElementsByClassName("work-panel");
+    var otherPanels = [];
+
+    for (var i = 0; i < panels.length; i++) {
+
+        if (panels[i].id == panelId) {
+            if (panels[i].style.display == "block") {
+                panels[i].style.display = "none";
+            }
+            else {
+                panels[i].style.display = "block";
+            }
+        }
+        else {
+            otherPanels.push(panels[i]);
+            for (var j = 0; j < otherPanels.length; j++) {
+                otherPanels[j].style.display = "none";
+            }
+        }
+    }
+}
+
+function hideOtherPanels(currentPanel) {
+    var panels = document.getElementsByClassName("work-panel");
+
+    for (var i = 0; i < panels.length; i++) {
+        if (panels[i] == currentPanel) {
+            console.log(`${panels[i]} == ${currentPanel}`);
+        }
+        else {
+            panels[i].style.display = "none";
         }
     }
 }
