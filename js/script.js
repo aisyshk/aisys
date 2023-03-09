68353cda7ccd2f26b255421026535626db708a39
無冬 <123821243+aisyshk@users.noreply.github.com>
2023-03-09 06:04:16 -0700

    spacers + license

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+/*
+
+    MIT License
+
+Copyright(c) 2023 Alice H.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+
+//////////////////////////////////////
+/// START: Site-wide Color Change ///
+////////////////////////////////////
+
 function forceChangeColor() {
     let colorScheme = localStorage.getItem("customColorScheme");
 
@@ -27,9 +58,64 @@ function resetDefaultColor() {
     localStorage.removeItem("customColorScheme");
 }
 
+document.getElementById("colorChanger").addEventListener("change", onColorChanged);
+
+function onColorChanged() {
+    let torgb = toRgb(this.value);
+    let split = torgb.split("|");
+
+    localStorage.setItem("customColorScheme", this.value);
+
+    document.documentElement.style.setProperty('--primary-color', `#ffffff`, "important");
+    document.documentElement.style.setProperty('--secondary-color', `#ffffff`, "important");
+    document.documentElement.style.setProperty('--text-secondary', `#ffffff`, "important");
+    document.documentElement.style.setProperty('--nav-button-border', `#ffffff`, "important");
+
+    changeColor(parseInt(split[0]), parseInt(split[1]), parseInt(split[2]));
+}
+
+function toHex(r, g, b) {
+    return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+}
+
+function toRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    var r = parseInt(result[1], 16);
+    var g = parseInt(result[2], 16);
+    var b = parseInt(result[3], 16);
+
+    return r + "|" + g + "|" + b;
+}
+
+function changeColor(r, g, b) {
+    let hex = toHex(r, g, b);
+    console.log(hex);
+    document.documentElement.style.setProperty('--primary-color', `${hex}`, "important");
+    document.documentElement.style.setProperty("--secondary-color", `${hex}`, "important");
+    document.documentElement.style.setProperty("--text-secondary", `${hex}`, "important");
+    document.documentElement.style.setProperty("--nav-button-border", `${hex}`, "important");
+}
+
+////////////////////////////////////
+/// END: Site-wide Color Change ///
+//////////////////////////////////
+
+/////////////////////////////
+/// START: Nav Dropdown  ///
+///////////////////////////
+
 function showDropdown() {
     document.getElementById("servicesDropdown").classList.toggle("show");
 }
+
+///////////////////////////
+/// END: Nav Dropdown  ///
+/////////////////////////
+
+///////////////////////////
+/// START: Accordions  ///
+/////////////////////////
 
 function displayWorkPanel() {
     if (document.getElementById("work-panel").style.display == "block") {
@@ -99,41 +185,6 @@ function hideOtherPanels(currentPanel) {
     }
 }
 
-document.getElementById("colorChanger").addEventListener("change", onColorChanged);
-
-function onColorChanged() {
-    let torgb = toRgb(this.value);
-    let split = torgb.split("|");
-
-    localStorage.setItem("customColorScheme", this.value);
-
-    document.documentElement.style.setProperty('--primary-color', `#ffffff`, "important");
-    document.documentElement.style.setProperty('--secondary-color', `#ffffff`, "important");
-    document.documentElement.style.setProperty('--text-secondary', `#ffffff`, "important");
-    document.documentElement.style.setProperty('--nav-button-border', `#ffffff`, "important");
-
-    changeColor(parseInt(split[0]), parseInt(split[1]), parseInt(split[2]));
-}
-
-function toHex(r, g, b) {
-    return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
-}
-
-function toRgb(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-
-    var r = parseInt(result[1], 16);
-    var g = parseInt(result[2], 16);
-    var b = parseInt(result[3], 16);
-
-    return r + "|" + g + "|" + b;
-}
-
-function changeColor(r, g, b) {
-    let hex = toHex(r, g, b);
-    console.log(hex);
-    document.documentElement.style.setProperty('--primary-color', `${hex}`, "important");
-    document.documentElement.style.setProperty("--secondary-color", `${hex}`, "important");
-    document.documentElement.style.setProperty("--text-secondary", `${hex}`, "important");
-    document.documentElement.style.setProperty("--nav-button-border", `${hex}`, "important");
-}
+/////////////////////////
+/// END: Accordions  ///
+///////////////////////
